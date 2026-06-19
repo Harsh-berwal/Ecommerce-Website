15c6a76ca467037b88dcaf735c5d5a430ff6ec74
Harsh <harshberwal240@gmail.com>
2026-06-19 14:07:06 +0530

    Fix Vercel build

--- ecommerce-project/vite.config.ts
+++ ecommerce-project/vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
-    },
-  })],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+  ],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001'
-      }
-    }
+      "/api": {
+        target: "http://localhost:3001",
+      },
+    },
   },
   build: {
-    outDir: '../ecommerce-backend/dist'
-  }
-})
+    outDir: "dist",
+  },
+});
